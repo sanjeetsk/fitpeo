ef460fcc1c01a3fa58aecc6b1d310b6886b628dd
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-10-14 23:39:26 +0530

    added

---
+++ src/component/bargraph/BarGraph.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BarGraph() {
+  return (
+    <div>
+      <h1>BarGraph</h1>
+    </div>
+  )
+}
+
+export default BarGraph
---
+++ src/component/piechart/Piechart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Piechart() {
+  return (
+    <div>
+      <h1>Piechart</h1>
+    </div>
+  )
+}
+
+export default Piechart
